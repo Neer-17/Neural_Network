a8006b11ba4be54e9039652e5e6d16b002ae17e6
Neeraj Panwar <neerajpanwar844@gmail.com>
2026-04-25 13:18:21 +0530

    Fixed display.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import numpy as np
-from neuralnet import *
-import streamlit as st
 import cv2
+import numpy as np
+import streamlit as st
+from neuralnet import Neural
 from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(page_title="DigitDetective",page_icon='🔍',layout='wide')
@@ -10,6 +10,7 @@ st.badge("numpy only - no frameworks")
 st.space('medium')
 @st.cache_data
 def load_model():
+    """Loads the Neural network."""
     n_net = Neural()
     n_net.load_weights()
     return n_net
@@ -17,6 +18,8 @@ def load_model():
 model = load_model()
 
 def process_img(img):
+    """Process the canvas result and returns a 1D array.\n
+    img : drawing/canvas result"""
     img = cv2.cvtColor(img.astype('uint8'),cv2.COLOR_RGBA2GRAY) 
     ret,thresh_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
     counters,hierarchy = cv2.findContours(thresh_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -30,18 +33,21 @@ def process_img(img):
     return norm_array.flatten()
 
 def predict(model,array):
+    """Predicts which number the given array is. """
     result = model.forward(array)
     predicted_digit = np.argmax(result)
     confidence = result
     return predicted_digit,confidence
 
-            
+if 'canvas_key' not in st.session_state:
+    st.session_state.canvas_key = 0           
+
 left_col, right_col = st.columns(2)
 left_col = left_col.container(border=True, height="stretch", vertical_alignment="top")
 with left_col:
     """## CANVAS"""
 
-    canvas_result = st_canvas(stroke_width=20,stroke_color="white",background_color="#000000",width=400,height=400,display_toolbar=False)
+    canvas_result = st_canvas(stroke_width=20,stroke_color="white",background_color="#000000",width=400,height=400,display_toolbar=False,key=f'canvas_{st.session_state.canvas_key}')
     button_cols = st.columns(2)
     with button_cols[1]:
         predict_btn = st.button("Predict", key="draw_predict")
@@ -53,8 +59,11 @@ with left_col:
         digit, confidence = predict(model,array)
         st.session_state['prediction'] = [digit,confidence]
     
-    elif clear_btn and len(canvas_result.json_data.get("objects")) > 0:
-        pass
+    if clear_btn:
+        st.session_state.canvas_key += 1 
+        if 'prediction' in st.session_state:
+            del st.session_state['prediction']
+        st.rerun()
 
 right_col = right_col.container(border=True, height="stretch", vertical_alignment="top")
 with right_col:
@@ -79,5 +88,6 @@ with right_col:
         with col3:
             st.metric('Accuracy','97.8%')
 
-
+    else:
+        st.markdown("**Draw a digit to see the prediction.**")
     
